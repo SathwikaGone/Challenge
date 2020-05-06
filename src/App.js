f67a898a090dfd44884b4a0d6d9efd985f57f4c1
SathwikaGone <gonesathwika@gmail.com>
2020-05-06 18:25:46 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Rewards from "./Components/Rewards";
 
+const data = [
+  {
+    fullName: "Mike",
+    customerID: 1,
+    transaction: [
+      {
+        amount: 60,
+        date: "01-01-2020",
+      },
+      {
+        amount: 180,
+        date: "02-01-2020",
+      },
+      {
+        amount: 20,
+        date: "02-01-2020",
+      },
+      {
+        amount: 72,
+        date: "03-01-2020",
+      },
+      {
+        amount: 1500,
+        date: "03-01-2020",
+      },
+    ],
+  },
+  {
+    fullName: "Petter",
+    customerID: 2,
+    transaction: [
+      {
+        amount: 1860,
+        date: "02-01-2020",
+      },
+      {
+        amount: 20,
+        date: "02-01-2020",
+      },
+      {
+        amount: 90,
+        date: "02-01-2020",
+      },
+      {
+        amount: 175,
+        date: "03-01-2020",
+      },
+      {
+        amount: 1480,
+        date: "03-01-2020",
+      },
+    ],
+  },
+];
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Rewards data={data} />
     </div>
   );
 }
---
+++ src/Components/Rewards.js
@@ -0,0 +1,101 @@
+import React, { Component } from "react";
+
+export default class Rewards extends Component {
+  state = {
+    name: "",
+    data: this.props.data,
+    Total: 0,
+    MonthRewards: [],
+    Months: ["January", "February", "March"],
+    showVal: false,
+    err: "",
+  };
+
+  handleChange = (e) => {
+    this.setState({ name: e.target.value, Total: 0, err: "", showVal: false });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { name, data } = this.state;
+    let monthNum = ["01", "02", "03"];
+    let MonthTot = [];
+
+    let userTransactions = data.filter(
+      (item) =>
+        item.fullName.toString().toLowerCase() === name.toString().toLowerCase()
+    );
+
+    if (userTransactions.length > 0) {
+      let total = this.ToTAmount(userTransactions[0].transaction);
+      for (let i = 0; i < 3; i++) {
+        let Monthly = userTransactions[0].transaction.filter(
+          (item) => item.date.toString().substring(0, 2) === monthNum[i]
+        );
+
+        MonthTot.push(this.ToTAmount(Monthly));
+      }
+      this.setState({ Total: total, MonthRewards: MonthTot, showVal: true });
+    } else {
+      this.setState({ err: "Name not Found" });
+    }
+  };
+
+  ToTAmount = (p) => {
+    let Total = p.reduce((acc, item) => {
+      let points;
+      if (item.amount < 50) {
+        points = 0;
+      } else if (item.amount <= 100) {
+        points = item.amount - 50;
+      } else {
+        points = 50 + (item.amount - 100) * 2;
+      }
+      return acc.concat(points);
+    }, []);
+    if (Total.length > 0) {
+      return Total.reduce((acc, cv) => acc + cv);
+    } else {
+      return 0;
+    }
+  };
+
+  render() {
+    const { name, err, Total, MonthRewards, showVal, Months } = this.state;
+    let i = 0;
+    let displayPoints;
+    if (showVal) {
+      displayPoints = (
+        <div>
+          <p>
+            3 months reward points:
+            {MonthRewards.map((item) => (
+              <p>
+                {Months[i++]}: {item}
+              </p>
+            ))}
+          </p>
+          <p> Total: {Total !== 0 ? Total : "N/A"}</p>
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>Name: </label>
+          <input
+            type="text"
+            value={this.state.name}
+            onChange={this.handleChange}
+          ></input>
+          <button type="submit">Submit</button>
+          <button type="reset">Reset</button>
+        </form>
+        <p> {name}</p>
+        <p style={{ color: "red" }}> {err}</p>
+        {displayPoints}
+      </div>
+    );
+  }
+}
