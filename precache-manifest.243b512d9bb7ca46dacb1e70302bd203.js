9ea11cc4c4e33d92e5f3b651ede131865803d767
SathwikaGone <gonesathwika@gmail.com>
2020-05-06 19:43:28 -0400

    Updates

---
+++ precache-manifest.243b512d9bb7ca46dacb1e70302bd203.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7042dacca99d214bcaa699d7620b7a5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e7bf7b8cfd6a6139cdc",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f149fd6bb33c49f65d01",
+    "url": "/static/js/2.416f10ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.416f10ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e7bf7b8cfd6a6139cdc",
+    "url": "/static/js/main.c002c684.chunk.js"
+  },
+  {
+    "revision": "87119526abcd3d3a18cf",
+    "url": "/static/js/runtime-main.13f77af9.js"
+  }
+]);
